07acb96349226d27f331a69b68c62d731bf4efca
sabujhasan21 <88270134+sabujhasan21@users.noreply.github.com>
2025-12-03 21:55:32 +0600

    Update love_animation.py

--- love_animation.py
+++ love_animation.py
@@ -57,7 +57,7 @@ html, body, [class*="block-container"] {
     <div id="gift">💝</div>
     <div id="open-text">Open the box</div>
     <audio autoplay loop>
-        <source src="https://www.youtube.com/shorts/86-tWQvyNso" type="audio/mpeg">
+        <source src="bg_music.mp3" type="audio/mpeg">
     </audio>
 </div>
 
@@ -180,5 +180,6 @@ gift.addEventListener('click', ()=>{
 """
 
 html(html_code, height=900)
+
 
 
